5b11d089015c17d6b3e9ab5e66e6195911fb9363
Allyn Nelson <Allynnelson13@gmail.com>
2018-01-23 21:18:47 -0700

    added begining of arduino side of comm will print out what is sent to it by pi (echoes)

---
+++ ArdCOMM/ArdCOMM/ArdCOMM.ino
@@ -0,0 +1,25 @@
+/*
+ Name:		ArdCOMM.ino
+ Created:	1/23/2018 8:52:45 PM
+ Author:	Allyn
+*/
+#include <TimeLib.h>
+String n;
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+	Serial.begin(9600);
+	setTime(12, 0, 0, 1, 1, 11);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() 
+{
+	if (Serial.available())
+	{
+		n = Serial.read() ;
+		Serial.println(n);
+	}
+	
+}
+
