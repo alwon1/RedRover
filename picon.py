3aaa8971bf3739d666a2b3746d7955db2d3d4036
Allyn Nelson <Allynnelson13@gmail.com>
2018-01-26 00:05:26 -0700

    more improvements should mostly work for sending and reciving generic data

--- picon.py
+++ picon.py
@@ -1,29 +1,42 @@
 import serial#imports commands for using serial ports
 import re
+import time
 print("initilizing connection")
-arduinoSerialData = serial.Serial('/dev/ttyUSB0',230400)
+arduinoSerialData = serial.Serial('/dev/ttyUSB0',9600)
 log = False
+x=1
 def ToArd(toard):#writes out the varibles value to the adrino
-	arduinoSerialData.write(toard)
+	arduinoSerialData.write((toard+"\n").encode())
 	logfile(toard)
 
 def screen():
-	while(arduinoSerialData.inWaiting() > 0):#reads data to console from adrino
+	while(arduinoSerialData.inWaiting() > 0):
+		print(arduinoSerialData.inWaiting())#reads data to console from adrino
 		pnd = (arduinoSerialData.readline())
 		logfile(pnd.decode('ASCII'))#we also create a log of the console as well as print to screen
-		print(pnd)
+		print(pnd.decode())
+	print(arduinoSerialData.inWaiting())
 
 def control():#switch for future use to modify script operation
-	if (arduinoSerialData.inWaiting()>0):
-		FromArd()#need to test both Fromard and tfromard if both work change to tfromard
-	if log == True:
-		log(test)#note test is untill return fromArd is improved to tell use what log we are#writing
-	else:
+	while x==1:
+	#if (arduinoSerialData.inWaiting()>0):
+		
+		#FromArd()#need to test both Fromard and tfromard if both work change to tfromard
+		#if log == True:
+			#log(test)#note test is untill return fromArd is improved to tell use what log we are#writing
+		#else:
 		screen()
-	#ToArd()
-	while arduinoSerialData.inWaiting()==0 and not input():
-		pass
-	control()
+		#ToArd()
+		y=True
+		while arduinoSerialData.inWaiting()==0 and y ==True:#and not input():
+			da=input()
+			if da:
+				y=False
+				print("you made it to d")
+				ToArd(da)
+				time.sleep(0.01)
+			#pass
+	#control()
 
 def log(name):
 	log = open(name,'a')#before calling check in waiting also use regexp to pick between print and log
@@ -37,10 +50,12 @@ def FromArd():
 		log = False
 
 def logfile(lg):
-	open("console_log",'a').write("\n" + lg)
+	open("/home/pi/Desktop/console_log",'a').write("\n" + lg)
 
 def tFromArd():#currently not used needs improvements
 	if re.search(r"?:--Log--",str(arduinoSerialData.readline())):
 		while not re.search(r"?:--endLog--",str(arduinoSerialData.readline())):
 			log(test)# add detection of what log is beng given
+
 control()
+
