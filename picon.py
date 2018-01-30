a7ae7a0385f4fdaf78fbbc69d1d4309e55009cb8
Allyn Nelson <Allynnelson13@gmail.com>
2018-01-29 21:12:00 -0700

    replaced picon with pycom2 sorting now works

--- picon.py
+++ picon.py
@@ -2,55 +2,60 @@ import serial#imports commands for using serial ports
 import re
 import time
 print("initilizing connection")
-arduinoSerialData = serial.Serial('/dev/ttyUSB0',9600)
+arduinoSerialData = serial.Serial('COM6',9600)#'/dev/ttyUSB0',9600)
 log = False
 x=1
+txt=""
 def ToArd(toard):#writes out the varibles value to the adrino
 	arduinoSerialData.write((toard+"\n").encode())
 	logfile(toard)
 
 def screen():
-	while(arduinoSerialData.inWaiting() > 0):
-		print(arduinoSerialData.inWaiting())#reads data to console from adrino
-		pnd = (arduinoSerialData.readline())
-		logfile(pnd.decode('ASCII'))#we also create a log of the console as well as print to screen
-		print(pnd.decode())
-	print(arduinoSerialData.inWaiting())
+	#while(arduinoSerialData.inWaiting() > 0):
+	#print(arduinoSerialData.inWaiting())#reads data to console from adrino
+	#pnd = (arduinoSerialData.readline())
+	logfile(txt)#pnd.decode('ASCII'))#we also create a log of the console as well as print to screen
+	print(txt)#.decode())
+	#print(arduinoSerialData.inWaiting())
 
 def control():#switch for future use to modify script operation
 	while x==1:
 	#if (arduinoSerialData.inWaiting()>0):
-		
-		#FromArd()#need to test both Fromard and tfromard if both work change to tfromard
-		#if log == True:
-			#log(test)#note test is untill return fromArd is improved to tell use what log we are#writing
-		#else:
-		screen()
+		while arduinoSerialData.inWaiting()>0:
+			FromArd()#need to test both Fromard and tfromard if both work change to tfromard
+			if log == True:
+				print("you")
+				log(test)#note test is untill return fromArd is improved to tell use what log we are#writing
+			else:
+				screen()
 		#ToArd()
 		y=True
 		while arduinoSerialData.inWaiting()==0 and y ==True:#and not input():
 			da=input()
 			if da:
 				y=False
-				print("you made it to d")
+				#print("you made it to d")
 				ToArd(da)
-				time.sleep(0.01)
+				time.sleep(0.1)
 			#pass
 	#control()
 
 def log(name):
-	log = open(name,'a')#before calling check in waiting also use regexp to pick between print and log
-	log.write(('\n' + arduinoSerialData.readline()).decode('ASCII'))
+	file = "C:\\Users\\Allyn\\Desktop\\"+name
+	log = open(file,'a')#before calling check in waiting also use regexp to pick between print and log
+	log.write('\n' + txt)
 
 def FromArd():
-	if bool(re.search(r"?:--Log--",str(arduinoSerialData.readline()))):# search for the string --log-- to indicate the start of a log
+	txt = str(arduinoSerialData.readline().decode('UTF-8'))
+	if bool(re.search(r"--Log--", txt)):# search for the string --log-- to indicate the start of a log
 		#perhaps turn into a while loop
 		log = True
-	elif bool(re.search(r"?:--endLog--",str(arduinoSerialData.readline()))):
+	elif bool(re.search(r"--endLog--",txt)):
 		log = False
 
 def logfile(lg):
-	open("/home/pi/Desktop/console_log",'a').write("\n" + lg)
+	with open("C:\\Users\\Allyn\\Desktop\\New Text Document",'a') as file:
+		file.write("\n" + lg)
 
 def tFromArd():#currently not used needs improvements
 	if re.search(r"?:--Log--",str(arduinoSerialData.readline())):
---
+++ pycom2.py
@@ -0,0 +1,64 @@
+
+import serial#imports commands for using serial ports
+import re
+import time
+print("initilizing connection")
+arduinoSerialData = serial.Serial('COM6',9600)#'/dev/ttyUSB0',9600)
+x = 1
+u = False
+def ToArd(toard):#writes out the varibles value to the adrino
+	arduinoSerialData.write((toard + "\n").encode())
+	logfile('s: ' + toard)
+
+def screen(toScreen):
+	log('console_Testlog',toScreen)
+	print(toScreen)
+
+def writeControl(rec,con):#conrols wether statment is writeen to screen or to log or filterd
+	global u
+	if rec == 1:
+		u = True
+	elif rec == 2:
+		u = False
+	elif rec == 3:
+		if u == True:
+			log('test',con)
+		else:
+			screen(con)
+	
+def control():#switch for future use to modify script operation
+	while x == 1:
+		while arduinoSerialData.inWaiting() > 0:
+			recived = arduinoSerialData.readline().decode()
+			writeControl(FromArd(recived),recived)
+
+		if arduinoSerialData.inWaiting() == 0:
+			da = input()
+			if da:
+				y = False
+				ToArd(da)
+				time.sleep(0.1)
+
+
+def log(name,toWrite):
+	with open("C:\\Users\\Allyn\\Desktop\\" + name + '.txt','a') as file:
+		file.write('\n' + toWrite)
+
+def FromArd(recived):
+	if re.search(r"--log--", recived,re.IGNORECASE):
+		return 1
+	elif re.search(r"--endlog--",recived, re.IGNORECASE):
+		return 2
+	else:
+		return 3
+
+
+def logfile(lg):
+	with open("C:\\Users\\Allyn\\Desktop\\New Text Document.txt",'a') as file:
+		file.write("\n" + lg)
+
+def tFromArd():#currently not used needs improvements
+	if re.search(r"?:--Log--",str(arduinoSerialData.readline())):
+		while not re.search(r"?:--endLog--",str(arduinoSerialData.readline())):
+			log(test)# add detection of what log is beng given
+control()
